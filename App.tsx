6e07eff49c7d26d29479175fbb887c5d270dbb01
Tunvir Rahman <tunvir.rahman07@gmail.com>
2020-03-22 12:36:31 +0600

    init

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import {View,SafeAreaView, Text} from "react-native";
+
+export default function App(){
+  return (<SafeAreaView style = {{backgroundColor:'blue'}}></SafeAreaView>
+  )
+}
